fb86717fe86c4aa56b26e1d36b35945b47f497eb
sean-323 <kimsiyeon5214@gmail.com>
2026-03-01 19:23:00 +0900

    3.01

---
+++ src/gold/G_2023신기한소수.java
@@ -0,0 +1,43 @@
+package gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class G_2023신기한소수 {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력
+        N = Integer.parseInt(br.readLine());
+
+        // 로직
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+    }
+
+    private static void DFS (int number, int jarisu) {
+        if (jarisu == N) {
+            if (isPrime(number)) System.out.println(number);
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) continue;
+
+            if (isPrime(number * 10 + i)) DFS(number * 10 + i, jarisu + 1);
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) return false;
+        }
+
+        return true;
+    }
+}
